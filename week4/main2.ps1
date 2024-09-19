8e6be68ee27bbf02a1be3671bcf71cd886bb24ad
Genesis <160640511+genbautista@users.noreply.github.com>
2024-09-19 16:26:00 -0400

    Update main2.ps1

--- week4/main2.ps1
+++ week4/main2.ps1
@@ -1,1 +1,11 @@
-
+function apacheLogs
+{
+param
+{
+    [string]$PageVisited,
+    [int]$httpCode,
+    [string]$browser
+}
+$ipAddress = $filteredData | Select-Object -ExcludeProperty IPAddress
+return $ipAddress
+}
