dcbe24a37f5aa13d0fde60eb5c92553267b543c9
Genesis <160640511+genbautista@users.noreply.github.com>
2024-11-06 17:54:31 -0500

    Add files via upload

--- week12/apacheLogMenu-1.bash
+++ week12/apacheLogMenu-1.bash
@@ -12,6 +12,9 @@ function displayOnlyIPs(){
 
 # function: displayOnlyPages:
 # like displayOnlyIPs - but only pages
+function displayOnlyPages(){
+cat "$logFile" | awk '{print $7}' | grep -E "^/" | sort | uniq -c
+}
 
 function histogram(){
 
@@ -40,12 +43,27 @@ function histogram(){
 # number and check with a condition whether it is greater than 10
 # the output should be almost identical to histogram
 # only with daily number of visits that are greater than 10 
+function frequentVisitors() {
+histogram | awk '$1 > 10'
+}
 
 # function: suspiciousVisitors
 # Manually make a list of indicators of attack (ioc.txt)
 # filter the records with this indicators of attack
 # only display the unique count of IP addresses.  
 # Hint: there are examples in slides
+function suspiciousVisitors(){
+iocFile="ioc.txt"
+logFile="/var/log/apache2/access.log.1"
+
+if [[ ! -f "${iocFile}" ]]
+then 
+echo "No indicator attacks"
+return 
+fi
+egrep -i -f "$iocFile" "$logFile" | awk '{print $1}' | sort | uniq -c
+}
+
 
 # Keep in mind that I have selected long way of doing things to 
 # demonstrate loops, functions, etc. If you can do things simpler,
@@ -56,10 +74,10 @@ do
 	echo "PLease select an option:"
 	echo "[1] Display all Logs"
 	echo "[2] Display only IPS"
-	# Display only pages visited
+	echo "[3] Display only visited pages"
 	echo "[4] Histogram"
-	# Frequent visitors
-	# Suspicious visitors
+	echo "[5] Frequent Visitors"
+	echo "[6] Suspicious Visitors"
 	echo "[7] Quit"
 
 	read userInput
@@ -78,14 +96,25 @@ do
 		displayOnlyIPs
 
 	# Display only pages visited
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying only pages visited:"
+		displayOnlyPages
 
 	elif [[ "$userInput" == "4" ]]; then
 		echo "Histogram:"
 		histogram
 
         # Display frequent visitors
+	elif [[ "$userInput" == "5" ]]; then 
+		echo "Frequent Visitors"
+		frequentVisitors
+
 	# Display suspicious visitors
+	elif [[ "$userInput" == "6" ]]; then 
+		echo "Suspicious Visitors"
+		suspiciousVisitors
 	# Display a message, if an invalid input is given
+	else
+		echo "Why did you input something outside of 1-7? Try again."
 	fi
 done
-
